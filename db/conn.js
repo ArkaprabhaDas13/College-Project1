894c507e04ee69839d2770137939c6b2409550a2
ArkaprabhaDas13 <84220013+ArkaprabhaDas13@users.noreply.github.com>
2022-11-07 20:59:33 +0530

    first commit

---
+++ db/conn.js
@@ -0,0 +1,12 @@
+
+
+
+// mongoose.connect("mongodb://localhost:27017/project",{
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true,
+//     useCreateIndex:true
+// }).then(()=>{
+//     console.log('connection successful');
+// }).catch((e)=>{
+//     console.log("no connection");
+// })
